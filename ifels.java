f368857f6f240e5378669b2e51e3bbc4b9950ce9
karravula chandra <94832445+chandrashivaji@users.noreply.github.com>
2022-12-27 14:08:38 +0530

    Create ifels.java

---
+++ ifels.java
@@ -0,0 +1,11 @@
+class ifels{
+  public static void main(String args[]){
+    Scanner a=new Scanner(System.in);
+    int e=a.nextInt();
+    if(e>18){
+      System.out.println("Eligible");
+    }else{
+      System.out.println("Not Eligible");
+    }
+  }
+}
