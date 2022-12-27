cf601f7d8c98bd032ec7bb864d6892c6db480d1f
karravula chandra <94832445+chandrashivaji@users.noreply.github.com>
2022-12-27 13:54:34 +0530

    Create input.java

---
+++ input.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+class input{
+  public static void main(String args[]){
+    Scanner a=new Scanner(System.in);
+    int b=a.nectInt();
+    char c=a.next.chatAt(0);
+    double d=d.nextDouble();
+    short s=a.nextShort();
+    long l=a.nextLong();
+    System.out.println("int "+b);
+    System.out.println("char "+c);
+    System.out.println("double "+d);
+    System.out.println("short "+s);
+    System.out.println("long "+l);
+  }
+}
+
+    
