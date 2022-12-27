559e7769d97b24a08de112f80fa4a42c1ae98281
karravula chandra <94832445+chandrashivaji@users.noreply.github.com>
2022-12-27 14:30:31 +0530

    Switchcase

---
+++ switchcase.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class switchcase{
+public static void main(String args[]){
+Scanner n=new Scanner(System.in);
+int a=n.nextInt();
+switch(a){
+ 	case 1: System.out.println("hello");
+		break;
+	case 2: System.out.println("hey guys");
+		break;
+	case 3: System.out.println("Hey man");
+		break;
+	case 4: System.out.println("chandra");
+		break;
+	default: System.out.println("Default");
+		break;
+}
+}
+}
+
+       
