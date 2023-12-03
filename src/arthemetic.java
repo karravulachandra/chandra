ae340c339174c00f5e28b091658942d16be10e87
chandrashivaji <karravullachandra2001@gmail.com>
2023-12-03 12:59:51 +0530

    java practice

--- src/arthemetic.java
+++ src/arthemetic.java
@@ -1,8 +1,22 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class arthemetic {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-
+        int a=sc.nextInt();
+        int b= sc.nextInt();
+        int result;
+        //addition
+        result=a+b;
+        System.out.println("addition"+result);
+        result=a-b;
+        System.out.println("subtraction"+result);
+        result=a/b;
+        System.out.println("division"+result);
+        result=a*b;
+        System.out.println("multiplication"+result);
+        result=a%b;
+        System.out.println("modulo"+result);
     }
 }
